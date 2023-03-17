1bc50fc4319c47989839f397d058f67d697913bd
Sunghoon Kang <hoon@akuity.io>
2023-03-17 13:35:15 +0900

    Allow client to set custom marshaller
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

--- pkg/grpc/gateway/options.go
+++ pkg/grpc/gateway/options.go
@@ -1,12 +1,22 @@
 package gateway
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+)
 
 type ClientOption func(*client)
 
 func WithHTTPClient(hc *http.Client) ClientOption {
 	return func(c *client) {
 		c.httpClient = hc
+	}
+}
+
+func WithMarshaller(m *runtime.JSONPb) ClientOption {
+	return func(c *client) {
+		c.marshaller = m
 	}
 }
 
