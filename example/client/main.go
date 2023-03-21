38ca1b4b439cfcf3897ce4e1f932bed7804258d1
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2023-03-21 10:08:39 -0700

    docs: add more details to README.md and usage example (#2)
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    Co-authored-by: Sunghoon Kang <hoon@akuity.io>

---
+++ example/client/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/akuity/grpc-gateway-client/pkg/grpc/gateway"
+
+	grpc_gateway_client_example "github.com/akuity/grpc-gateway-client-example/pkg/api/gen"
+)
+
+func main() {
+	baseURL := "http://localhost:8081"
+	if len(os.Args) > 1 {
+		baseURL = os.Args[1]
+	}
+	client := grpc_gateway_client_example.NewGreeterGatewayClient(gateway.NewClient(baseURL))
+	resp, err := client.SayHello(context.Background(), &grpc_gateway_client_example.HelloRequest{Name: "World"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp.Message)
+}
---
+++ example/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"net"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	gen "github.com/akuity/grpc-gateway-client-example/pkg/api/gen"
+)
+
+type greeter struct {
+	gen.UnimplementedGreeterServer
+}
+
+func (g *greeter) SayHello(_ context.Context, req *gen.HelloRequest) (*gen.HelloReply, error) {
+	return &gen.HelloReply{Message: "Hello " + req.Name}, nil
+}
+
+func main() {
+	ctx := context.TODO()
+	srv := grpc.NewServer()
+	gen.RegisterGreeterServer(srv, &greeter{})
+
+	grpcListener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	if err := gen.RegisterGreeterHandlerFromEndpoint(ctx, mux, "localhost:8080", opts); err != nil {
+		panic(err)
+	}
+
+	go srv.Serve(grpcListener)
+	go http.ListenAndServe(":8081", mux)
+	println("Listening on :8081")
+	<-ctx.Done()
+}
